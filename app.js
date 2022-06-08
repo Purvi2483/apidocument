acdc36082c6d8984e0d306e775f9c72ecfd112dc
Purvi Gawande <94441247+Purvi2483@users.noreply.github.com>
2022-06-09 02:29:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,7 @@ app.get('/location',(req,res)=> {
 
 
 
-//rest wrt quicksearch
-app.get('/restaurants',(req,res)=> {
-    let restId =Number(req.params.id );
- db.collection('restaurants').find({state_id:restId}).toArray((err,result)=>{
-        if (err) throw err;
-        res.send(result)   
-    }
-   )
-   
-})
+
 app.get('/restaurants',(req,res)=> {
     let restId =Number(req.query.state_id )
     let mealId = Number(req.query.meal_id)
